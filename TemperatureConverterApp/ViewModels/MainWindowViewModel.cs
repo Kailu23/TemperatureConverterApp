d53e542ef10327a94c1bf08bdbe1d5ad2d04737e
Marin Valenčak <valencakmarin@gmail.com>
2026-01-25 02:45:25 +0100

    Add control, layout and resize window in AvaloniaUI
    	- Add a clickable Calculate control button
    	- Add Celsius & Fahrenheit text and input text boxes
    	- Resize the windows so it mimics a mobile device look

---
+++ TemperatureConverterApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TemperatureConverterApp.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
