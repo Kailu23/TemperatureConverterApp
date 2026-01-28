e9aada9e68ba0da91b852ea46b57112f7d1751c0
Marin Valenčak <valencakmarin@gmail.com>
2026-01-28 03:34:05 +0100

    Implement conversion from Fahrenheit to Celsius

--- TemperatureConverterApp/Views/MainWindow.axaml.cs
+++ TemperatureConverterApp/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using System;
+using System.Diagnostics;
 
 namespace TemperatureConverterApp.Views {
     public partial class MainWindow : Window {
@@ -7,15 +8,39 @@ namespace TemperatureConverterApp.Views {
             InitializeComponent();
         }
         private void CelsiusTextChanged(object? sender, TextChangedEventArgs e) {
-            if (String.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-") {
-                Fahrenheit.Text = "";
-            } else if (double.TryParse(Celsius.Text, out double celsius)) {
-                double fahrenheit = celsius * (9 / 5) + 32;
-                Fahrenheit.Text = fahrenheit.ToString("0.0");
-            } else {
-                Fahrenheit.Text = "0";
-                Celsius.Text = "0";
+            if (Celsius.IsFocused) {
+                if (IsTextValid(Celsius.Text)) {
+                    Fahrenheit.Text = "";
+                } else if (double.TryParse(Celsius.Text, out double celsius)) {
+                    double fahrenheit = celsius * (9 / 5) + 32;
+                    Fahrenheit.Text = fahrenheit.ToString("0.0");
+                } else {
+                    InvalidInput();
+                }
             }
+        }
+
+        private void FahrenheitTextChanged(object? sender, TextChangedEventArgs e) {
+            if (Fahrenheit.IsFocused) {
+                if (IsTextValid(Fahrenheit.Text)) {
+                    Celsius.Text = "";
+                } else if (double.TryParse(Fahrenheit.Text, out double fahrenheit)) {
+                    double celsius = (fahrenheit - 32) / (9 / 5);
+                    Debug.WriteLine(celsius);
+                    Celsius.Text = celsius.ToString("0.0");
+                } else {
+                    InvalidInput();
+                }
+            }
+        }
+
+        private bool IsTextValid(string? text) {
+            return String.IsNullOrEmpty(text) || text == "-";
+        }
+
+        private void InvalidInput() {
+            Fahrenheit.Text = "0";
+            Celsius.Text = "0";
         }
     }
 }
