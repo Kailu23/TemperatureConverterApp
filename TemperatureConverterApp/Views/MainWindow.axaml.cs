e8fb531ecf7785e9ecffce4145052e0dc4896e25
Marin Valenčak <valencakmarin@gmail.com>
2026-01-28 03:08:11 +0100

    Finish tutorial for Avalonia UI framework
    	- Implement conversion from celsius to fahrenheit as the user
    	  types
    	- Fahrenheit text box to read only.

--- TemperatureConverterApp/Views/MainWindow.axaml.cs
+++ TemperatureConverterApp/Views/MainWindow.axaml.cs
@@ -1,12 +1,21 @@
 using Avalonia.Controls;
+using System;
 
-namespace TemperatureConverterApp.Views
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace TemperatureConverterApp.Views {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
+        }
+        private void CelsiusTextChanged(object? sender, TextChangedEventArgs e) {
+            if (String.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-") {
+                Fahrenheit.Text = "";
+            } else if (double.TryParse(Celsius.Text, out double celsius)) {
+                double fahrenheit = celsius * (9 / 5) + 32;
+                Fahrenheit.Text = fahrenheit.ToString("0.0");
+            } else {
+                Fahrenheit.Text = "0";
+                Celsius.Text = "0";
+            }
         }
     }
 }
